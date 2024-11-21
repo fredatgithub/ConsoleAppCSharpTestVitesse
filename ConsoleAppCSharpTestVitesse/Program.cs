350e30ed3210919a3d3bde548b9b402b9e86ff66
fred <domaine75@gmail.com>
2024-11-21 14:21:31 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppCSharpTestVitesse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAppCSharpTestVitesse
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      double result = 0;
+
+      for (int i = 0; i < 1_000_000_000; ++i)
+      {
+
+        result += i * 0.5;
+
+      }
+
+      Console.WriteLine(result);
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
---
+++ ConsoleApplicationCppTestVitesse/ConsoleApplicationCppTestVitesse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "test vitesse C++\n";
+    double result = 0;
+
+    for (int i = 0; i < 100000000; ++i) {
+
+      result += i * 0.5;
+
+    }
+
+    std::cout << result << std::endl;
+
+    return 0;
+}
